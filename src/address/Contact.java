80f40413f9c9b81fc9b82352b8fc9cd52a82e97c
stephenkumalo <stephenkumalo@live.com>
2015-05-30 23:32:48 -0500

    Contact class and an empty implentation file

---
+++ src/address/Contact.java
@@ -0,0 +1,33 @@
+package address;
+
+public class Contact {
+protected String firstName;
+protected String lastName;
+protected String email;
+protected String address;
+
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+}
